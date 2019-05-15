d0680acae7872f6acf673b23e3d74e21dfa7f86c
Revina Laksmi Permata Hati <revinaaniver@gmail.com>
2019-05-15 14:28:01 +0700

    template email

---
+++ resources/views/email/lolosfinal.blade.php
@@ -0,0 +1,25 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+</head>
+<body>
+<p>Hello {{ $nama }},</p>
+<p> 
+	Dengan ini kami menginformasikan bahwa Anda lolos seleksi dan diterima sebagai Siswa Magang di PT. Digdaya Olah Teknologi Indonesia (DOT Indonesia) sebagai {{ $posisi }} mulai dari tanggal {{$tgl_awal}} hingga {{$tgl_akhir}}.
+
+	Kami tunggu kehadirannya di hari pertama magang di kantor Malang PT. Digdaya Olah Teknologi Indonesia di Perumahan Permata Hijau A15 Kelurahan Tlogomas Kecamatan Lowokwaru Kota Malang. 
+	Lokasi Google Maps : https://goo.gl/maps/CmMXiAprPPq
+
+	Selamat bergabung dengan keluarga kami.
+	Mari Berkolaborasi 😁
+</p>
+<p>
+    Misbakhul Mustofin
+    <br>
+    Manager Magang
+    <br>
+    PT. Digdaya Olah Teknologi Indonesia
+</p>
+</body>
+</html>
