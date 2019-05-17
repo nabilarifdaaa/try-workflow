cfbcd1285eea6ba1290087c587dc78f022913868
LAPTOP-4CTHJB70\Asus <nabilarifdaaa@gmail.com>
2019-05-17 10:37:12 +0700

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 
 class HomeController extends Controller
 {
@@ -25,7 +26,25 @@ class HomeController extends Controller
     public function index()
     {
         $admin=Auth::user();
-        // dd($admin);
-        return view('home', compact('admin'));
+        $count_regis = DB::table('states')
+            ->where('states.state','=','registered')
+            ->count();
+        $count_acc = DB::table('states')
+            ->where('states.state','=','accepted')
+            ->count();
+        $count_reject = DB::table('states')
+            ->where('states.state','=','rejected')
+            ->count();
+        $count_review = DB::table('states')
+            ->where('states.state','=','review_cv')
+            ->count();
+        $count_tes = DB::table('states')
+            ->where('states.state','=','technical_test')
+            ->count();
+        $count_interview = DB::table('states')
+            ->where('states.state','=','interview')
+            ->count();
+
+        return view('home', compact('admin','count_regis','count_acc','count_reject','count_review','count_tes','count_interview'));
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,6 +15,7 @@
       <div class="col-lg-4">
         <div class="card card-small mb-4 pt-3 text-center">
           <div class="card-header border-bottom text-center">
+            <p>Hello Admin!</p>
             <div class="mb-3 mx-auto">
               <img class="rounded-circle" src="{{asset('frontend/images/teamwork.png')}}  " alt="User Avatar" width="110"> </div>
             <h4 class="mb-0">{{$admin->name}}</h4>
@@ -22,8 +23,107 @@
             {{-- <button type="button" class="mb-2 btn btn-sm btn-pill btn-outline-primary mr-2">
               <i class="material-icons mr-1">person_add</i>Follow</button> --}}
           </div>
-          
         </div>
       </div>
+    </div>
+    <div class="row">
+        <div class="col-lg col-md-6 col-sm-6 mb-4">
+          <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+              <div class="d-flex flex-column m-auto">
+                <div class="stats-small__data text-center">
+                  <span class="stats-small__label text-uppercase">Calon Magang</span>
+                  <h6 class="stats-small__value count my-3">{{$count_regis}}</h6>
+                </div>
+                <div class="stats-small__data">
+                  <span class="badge badge-pill badge-secondary">Registered</span>
+                </div>
+              </div>
+              <canvas height="120" class="blog-overview-stats-small-1"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg col-md-4 col-sm-6 mb-4">
+          <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+              <div class="d-flex flex-column m-auto">
+                <div class="stats-small__data text-center">
+                  <span class="stats-small__label text-uppercase">Calon Magang</span>
+                  <h6 class="stats-small__value count my-3">{{$count_review}}</h6>
+                </div>
+                <div class="stats-small__data">
+                  <span class="badge badge-pill badge-secondary">Review CV</span>
+                </div>
+              </div>
+              <canvas height="120" class="blog-overview-stats-small-3"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg col-md-4 col-sm-6 mb-4">
+          <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+              <div class="d-flex flex-column m-auto">
+                <div class="stats-small__data text-center">
+                  <span class="stats-small__label text-uppercase">Calon Magang</span>
+                  <h6 class="stats-small__value count my-3">{{$count_tes}}</h6>
+                </div>
+                <div class="stats-small__data">
+                  <span class="badge badge-pill badge-secondary">Tes Teknis</span>
+                </div>
+              </div>
+              <canvas height="120" class="blog-overview-stats-small-3"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg col-md-4 col-sm-6 mb-4">
+          <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+              <div class="d-flex flex-column m-auto">
+                <div class="stats-small__data text-center">
+                  <span class="stats-small__label text-uppercase">Calon Magang</span>
+                  <h6 class="stats-small__value count my-3">{{$count_interview}}</h6>
+                </div>
+                <div class="stats-small__data">
+                  <span class="badge badge-pill badge-secondary">Wawancara</span>
+                </div>
+              </div>
+              <canvas height="120" class="blog-overview-stats-small-3"></canvas>
+            </div>
+          </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg col-md-6 col-sm-6 mb-4">
+          <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+              <div class="d-flex flex-column m-auto">
+                <div class="stats-small__data text-center">
+                  <span class="stats-small__label text-uppercase">Calon Magang</span>
+                  <h6 class="stats-small__value count my-3">{{$count_acc}}</h6>
+                </div>
+                <div class="stats-small__data">
+                  <span class="badge badge-pill badge-success">Accepted</span>
+                </div>
+              </div>
+              <canvas height="120" class="blog-overview-stats-small-2"></canvas>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg col-md-4 col-sm-6 mb-4">
+          <div class="stats-small stats-small--1 card card-small">
+            <div class="card-body p-0 d-flex">
+              <div class="d-flex flex-column m-auto">
+                <div class="stats-small__data text-center">
+                  <span class="stats-small__label text-uppercase">Calon Magang</span>
+                  <h6 class="stats-small__value count my-3">{{$count_reject}}</h6>
+                </div>
+                <div class="stats-small__data">
+                  <span class="badge badge-pill badge-danger">Rejected</span>
+                </div>
+              </div>
+              <canvas height="120" class="blog-overview-stats-small-3"></canvas>
+            </div>
+          </div>
+        </div>
     </div>
 @endsection
